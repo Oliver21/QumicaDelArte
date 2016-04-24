0e8c9e1bd523301b2904108691c57e8b3632f9a0
Oliver <oliver.mtz10@gmail.com>
2016-04-24 17:07:52 -0500

    Se creo la pagina de login, home page y leccions, aun falta agregar funcionalidad y diseno

---
+++ html/home.php
@@ -0,0 +1,20 @@
+<html>
+<link rel="stylesheet" type="text/css" href="../css/homeStyle.css">
+<script src="../js/jquery.js" type="text/javascript"> </script>
+<link href="../images/logo1.png" rel="shortcut icon">
+<head>
+<title>Qumica del Arte</title>
+</head>
+<body>
+	<center>
+	<img  id="texto" src="../images/texto2.gif"></img> <br>
+		<img  id="pint" src="../images/pintura.gif"></img>
+	<p id="intro">Bienvenido a la "Química del Arte", en la parte inferior de esta página encontraras distintos botones que te
+	permitiran acceder a una leccion sobre la química, cuestionarios y una simulacion sobre el arte y la química </p>
+	<br><br>
+	<input type="submit" id="leccion" class="button" value="Aprende" onclick="login()"></input>
+	<input type="submit" id="preguntas" class="button" value="Preguntas" onclick="login()"></input>
+	<input type="submit" id="simulacion" class="button" value="Simulación" onclick="login()"></input>
+</center>
+</body>
+</html>
---
+++ html/login.php
@@ -0,0 +1,66 @@
+<html>
+<link rel="stylesheet" type="text/css" href="../css/loginStyle.css">
+<script src="../js/jquery.js" type="text/javascript"> </script>
+<link href="../images/logo1.png" rel="shortcut icon">
+<head>
+<title>Qumica del Arte</title>
+</head>
+<body>
+<center>
+<div class="titulo">Química del Arte</div>
+<mensaje>Aprende mientras te diviertes</mensaje>
+<br><br><br><br>
+
+<input type="text" placeholder="Usuario" name="usuario1" style="background:transparent; width:250px; height:40px; font-size:18px;" id="usuario"></input>
+<br><br><br>
+<input type="password" placeholder="Contraseña" name="password" style="background:transparent; width:250px; height:40px; font-size:18px;" id="pass"></input>
+<br><br><br>
+<input type="submit" id="login" class="button" value="login" onclick="login()"></input>
+<br><br>
+
+<!--<a href="forgot.html">Forgot password?</a>-->
+<!--<form action= "register.html" method="POST"> 
+<input type ="submit" class="button2" value="Register"></input> 
+</form> -->
+<img  id="cuadro" src="../images/cuadro.png"></img>
+</center>
+
+<script type="text/javascript">
+
+				function login(){
+                var jsonObject = {
+                    "username" : $("#usuario").val(),
+                    "userPassword" : $("#pass").val()
+                };
+
+                $.ajax({
+                    type: "POST",
+                    url: "../php/loginService.php",
+                    dataType: "json",
+                    data: jsonObject,
+                    headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                    success: function(jsonData) {
+                        alert("Welcome again " + jsonData.name + " " + "\n Your password is safe with us.");
+                            window.location.replace("../Html/home.php");
+
+                       
+                    },
+                    error: function(errorMsg){
+                        alert(errorMsg.statusText);
+                    }
+                });
+            }
+
+            
+      /*      $("#registerButton").on("click",function(){
+                window.location.replace("PHP%20Service/registration.html");
+            }); */
+
+
+
+
+    </script>
+
+
+</body>
+</html>
